c816b1a271d8b236e132dff38cea16786ef1ccd9
sathiyatskrj <76795532+sathiyatskrj@users.noreply.github.com>
2025-12-19 04:27:36 +0530

    Rename play method to playAsset in AudioService

--- lib/services/audio_service.dart
+++ lib/services/audio_service.dart
@@ -3,14 +3,10 @@ import 'package:audioplayers/audioplayers.dart';
 class AudioService {
   final AudioPlayer _player = AudioPlayer();
 
-  Future<void> play(String assetPath) async {
-    try {
-      await _player.stop();
-      await _player.play(
-        AssetSource(assetPath.replaceFirst('assets/', '')),
-      );
-    } catch (e) {
-      print('Audio play error: $e');
-    }
+  Future<void> playAsset(String assetPath) async {
+    await _player.stop();
+    await _player.play(
+      AssetSource(assetPath.replaceFirst('assets/', '')),
+    );
   }
 }
