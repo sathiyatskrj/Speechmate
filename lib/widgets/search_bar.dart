f555bfd1d7bca4de0f01d12120663d30cc481cb8
sathiyatskrj <76795532+sathiyatskrj@users.noreply.github.com>
2025-12-22 20:48:43 +0530

    Update search_bar.dart

--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -4,23 +4,18 @@ class Search extends StatelessWidget {
   final TextEditingController controller;
   final VoidCallback onSearch;
   final VoidCallback onClear;
-  final VoidCallback? onVoiceSearch;
 
   const Search({
     super.key,
     required this.controller,
     required this.onSearch,
     required this.onClear,
-    this.onVoiceSearch,
   });
 
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
-    final isSmallScreen = screenWidth < 360;
-    
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: isSmallScreen ? 12 : 16),
+      padding: const EdgeInsets.symmetric(horizontal: 16),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(30),
@@ -37,32 +32,26 @@ class Search extends StatelessWidget {
           Expanded(
             child: TextField(
               controller: controller,
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 hintText: "Search words",
-                hintStyle: TextStyle(
-                  color: Colors.black45,
-                  fontSize: isSmallScreen ? 14 : 16,
-                ),
+                hintStyle: TextStyle(color: Colors.black45),
                 border: InputBorder.none,
               ),
-              style: TextStyle(fontSize: isSmallScreen ? 14 : 16),
-              onChanged: (_) {},
+              onSubmitted: (_) => onSearch(), // Enable Enter key search
+              textInputAction: TextInputAction.search,
             ),
           ),
           IconButton(
             icon: const Icon(Icons.search),
             onPressed: onSearch,
-            iconSize: isSmallScreen ? 20 : 24,
           ),
           if (controller.text.isNotEmpty)
             IconButton(
               icon: const Icon(Icons.close),
               onPressed: onClear,
-              iconSize: isSmallScreen ? 20 : 24,
             ),
         ],
       ),
     );
   }
 }
-
