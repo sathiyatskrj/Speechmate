d36a28db2d5adbb86a07c06fa7e8d7948681c6bd
sathiyatskrj <76795532+sathiyatskrj@users.noreply.github.com>
2025-12-22 20:37:17 +0530

    Update background.dart

--- lib/widgets/background.dart
+++ lib/widgets/background.dart
@@ -1,22 +1,33 @@
 import 'package:flutter/material.dart';
 
 class Background extends StatelessWidget {
-  final List<Color> colors;
   final Widget child;
+  final List<Color> colors;
   final EdgeInsetsGeometry? padding;
 
   const Background({
     super.key,
-    required this.colors,
     required this.child,
+    this.colors = const [],
     this.padding,
   });
 
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    final isSmallScreen = screenSize.width < 360;
+    final isMediumScreen = screenSize.width < 400;
+    
+    // Responsive padding based on screen size
+    final responsivePadding = padding ?? EdgeInsets.symmetric(
+      horizontal: isSmallScreen ? 12 : (isMediumScreen ? 16 : 20),
+      vertical: isSmallScreen ? 30 : (isMediumScreen ? 40 : 50),
+    );
+
     return Container(
       width: double.infinity,
       height: double.infinity,
+      padding: responsivePadding,
       decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: colors,
@@ -24,12 +35,7 @@ class Background extends StatelessWidget {
           end: Alignment.bottomCenter,
         ),
       ),
-      child: SafeArea(
-        child: Padding(
-          padding: padding ?? EdgeInsets.zero,
-          child: child,
-        ),
-      ),
+      child: child,
     );
   }
 }
