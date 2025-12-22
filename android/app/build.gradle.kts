8fb95d1c8fd31ac67d6b4a3cd24bfd0f8c4f1bd8
sathiyatskrj <76795532+sathiyatskrj@users.noreply.github.com>
2025-12-22 12:40:20 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,13 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.speechmate.speechmate"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 34  // Updated to latest stable
+    ndkVersion = "27.0.12077973"  // Fixed NDK version
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -20,20 +19,15 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.speechmate.speechmate"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        minSdk = 21  // Android 5.0 (Lollipop) - supports old devices
+        targetSdk = 34  // Latest stable Android
+        versionCode = 1
+        versionName = "1.2.0"
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
